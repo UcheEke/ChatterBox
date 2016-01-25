f86cb565a7c44c7171f1d2e008a9fb1306a95e7b
Uche Eke <uche.eke@gmail.com>
2016-01-25 16:54:31 +0000

    Added client and chat room functionality

--- main.go
+++ main.go
@@ -27,9 +27,14 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 func main() {
 
 	addr := ":8080"
+	r := newRoom()
 
-	// Function
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// Start the room running as a concurrent process
+	go r.run()
+
 	// Start the web server
 	fmt.Printf("Starting web server on localhost%s...\n", addr)
 	if err := http.ListenAndServe(addr, nil); err != nil {
